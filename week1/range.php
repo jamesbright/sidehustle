5056bce31e957b2a144e7b4fc49e6ab6d1237fd2
James Ononiwu <jamesononiwu@gmail.com>
2021-06-24 14:58:32 +0100

    ...

---
+++ week1/range.php
@@ -0,0 +1,24 @@
+<?php
+
+function getRange($start,$end,$arr){
+//if array is empty terminate execution
+if(count($arr) == 0){echo 'given array is empty'; return; }
+
+//a new array to hold selected values
+$range_values = array();
+
+//push values from the given array into the new array using the start and end range
+for($start; $start <= $end; $start++){
+    array_push($range_values, $arr[$start]);
+}
+
+//return the selected values
+return $range_values;
+}
+
+//testing
+$start = 1;
+$end = 4;
+$arr = [5,6,9,8,2,3,5,0];
+print_r(getRange($start,$end,$arr));
+?>
---
+++ week1/sum.php
@@ -0,0 +1,22 @@
+<?php
+
+function sum($arr){
+//if array is empty terminate execution
+if(count($arr) == 0){echo 'given array is empty'; return; }
+
+//variable to hold summed values
+$sum = 0;
+
+//iterate through the array and sum the values
+for($i = 0; $i < count($arr); $i++){
+    $sum = $sum + $arr[$i];
+}
+
+//return the summed value
+return $sum;
+}
+
+//testing
+$arr = [5,6,9,8,2,3,5,0];
+print_r(sum($arr));
+?>
